bba90d724ed333e056d08e1d4178427fa00552a0
Tona-Mangobyte <tona@mango-byte.com>
2023-04-09 10:39:12 +0700

    prepare

--- src/simple_moment.js
+++
@@ -1,8 +0,0 @@
-// import moment from "moment";
-const moment = require("moment");
-
-const now = moment();
-const closingDate = '2023-03-20';
-const momentClosingDate = moment(closingDate)
-const result = momentClosingDate.isAfter(now)
-console.log(result);
